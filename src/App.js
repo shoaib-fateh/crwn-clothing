2cf19d96951a2d0232d1b4db2762a560540c12b9
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-07-07 15:46:06 +0430

    init Home Page JSX code and Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import "./App.css";
+import "./homepage.styles.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Crwn Clothing!</h1>
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          {/* <img /> */}
+          <div className="content">
+            <h2 className="title">Hats</h2>
+            <p className="subtitle">Shop Now</p>
+          </div>
+        </div>
+        <div className="menu-item">
+          {/* <img /> */}
+          <div className="content">
+            <h2 className="title">Jackets</h2>
+            <p className="subtitle">Shop Now</p>
+          </div>
+        </div>
+        <div className="menu-item">
+          {/* <img /> */}
+          <div className="content">
+            <h2 className="title">Sneakers</h2>
+            <p className="subtitle">Shop Now</p>
+          </div>
+        </div>
+        <div className="menu-item">
+          {/* <img /> */}
+          <div className="content">
+            <h2 className="title">Womens</h2>
+            <p className="subtitle">Shop Now</p>
+          </div>
+        </div>
+        <div className="menu-item">
+          {/* <img /> */}
+          <div className="content">
+            <h2 className="title">Mens</h2>
+            <p className="subtitle">Shop Now</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 export default App;
