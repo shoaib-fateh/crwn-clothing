1fd42fec6a432b9708657bc2407a3d415d770119
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-07-10 09:25:28 +0430

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import styled from "styled-components";
+import Header from "./components/header/header.component";
 const App = () => {
   return (
     <MainAppStyled>
+      <Header />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/shop" Component={ShopPage} />
@@ -17,8 +19,13 @@ const App = () => {
 };
 
 const MainAppStyled = styled.div`
-  font-family: "Open sans Condensed";
+  font-family: "Open sans Condensed", sans-serif;
   padding: 20px 80px;
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
 `;
 
 export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <div className="logo-container">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+      </div>
+
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </HeaderStyled>
+  );
+};
+
+const HeaderStyled = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+
+  .logo-container {
+    height: 100%;
+    width: 70px;
+    padding: 25px;
+  }
+
+  .options {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    .option {
+      padding: 10px 15px;
+    }
+  }
+`;
+
+export default Header;
