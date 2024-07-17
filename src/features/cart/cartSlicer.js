be93ced61599513b4fb50ec03dea319277db6ad1
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-07-17 10:00:55 +0430

    add carticon and cart deropdown components to our application

--- src/features/cart/cartSlicer.js
+++ src/features/cart/cartSlicer.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    hidden: false,
+    hidden: true,
   },
   reducers: {
     toggleCartHidden: (state) => {
