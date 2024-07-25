4a06cec2e3e848fd1783930cecaa709ba259dbd2
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-07-25 08:19:47 +0430

    coded our server .js for starting point

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const corse = require("cors");
+
 const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(corse());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -20,23 +22,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, (error) => {
-  if (error) throw error;
-  console.log("Server running on port " + port);
-});
-
-app.post("/payment", (req, res) => {
-  const body = {
-    source: req.body.token.id,
-    amount: req.body.amount,
-    currency: "usd",
-  };
-
-  stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
-    } else {
-      res.status(200).send({ success: stripeRes });
-    }
-  });
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server Started On Port " + port);
 });
